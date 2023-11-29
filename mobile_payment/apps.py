663b6305d7c3c6a677a474cb0bd167785b833dc1
Abdul Muizz <azikumapayi@gmail.com>
2023-11-29 16:36:21 +0000

    sligt update on some spacing and naming styles

--- mobile_payment/apps.py
+++ mobile_payment/apps.py
@@ -10,7 +10,7 @@ DEFAULT_CFG = {
       "gql_mutation_update_payment_transaction_perms":["208002"],
       "gql_query_verify_insuree_perms":["208003"],
       "gql_mutation_process_payment_perms":["208004"],
-      "gql_query_payment_service_provider":["208005"],
+      "gql_query_payment_service_provider_perms":["208005"],
       "gql_mutation_create_payment_service_provider_perms":["208006"],
       "gql_mutation_update_payment_service_provider_perms":["208007"],
       "gql_mutation_delete_payment_service_provider_perms":["208008"],
@@ -22,7 +22,7 @@ class MobilepaymentConfig(AppConfig):
     name = 'mobile_payment'
     gql_query_payment_transaction_perms = []
     gql_mutation_create_payment_transaction_perms =[]
-    gql_query_payment_service_provider =   []
+    gql_query_payment_service_provider_perms =   []
     gql_mutation_update_payment_transaction_perms = []
     gql_query_verify_insuree_perms = []
     gql_mutation_process_payment_perms = []
@@ -32,7 +32,7 @@ class MobilepaymentConfig(AppConfig):
 
     def _configure_perms(self, cfg):
           MobilepaymentConfig.gql_query_payment_transaction_perms = cfg["gql_query_payment_transaction_perms"]
-          MobilepaymentConfig.gql_query_payment_service_provider  = cfg["gql_query_payment_service_provider"]
+          MobilepaymentConfig.gql_query_payment_service_provider_perms  = cfg["gql_query_payment_service_provider_perms"]
           MobilepaymentConfig.gql_mutation_create_payment_transaction_perms = cfg["gql_mutation_create_payment_transaction_perms"]
           MobilepaymentConfig.gql_mutation_update_payment_transaction_perms = cfg["gql_mutation_update_payment_transaction_perms"]
           MobilepaymentConfig.gql_query_verify_insuree_perms = cfg["gql_query_verify_insuree_perms"]
